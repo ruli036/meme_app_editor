0dcfaa058c0c179e84cc07c840d531ab574bedfa
ruli andrian <tumbon04082000@gmail.com>
2025-07-06 09:09:10 +0700

    change size circular loading

--- lib/presentation/modules/memegallery/widget/item_image.dart
+++ lib/presentation/modules/memegallery/widget/item_image.dart
@@ -6,46 +6,55 @@ import 'package:meme_editor_app/data/model/meme_image_model.dart';
 
 class ItemImage extends StatelessWidget {
   final MemeImageModel item;
-  const ItemImage({super.key,required this.item});
+
+  const ItemImage({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => Get.toNamed(AppRoutes.detailImage,arguments: item),
+      onTap: () => Get.toNamed(AppRoutes.detailImage, arguments: item),
       child: Stack(
-          children: [
-            Center( 
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: CachedNetworkImage(
-                  imageUrl: item.url,
-                  cacheKey:item.id,
-                  fit: BoxFit.cover,
-                  errorWidget: (context, url, error) => Center(child: Icon(Icons.image_not_supported)),
-                  placeholder: (context, url) => Center(child: CircularProgressIndicator()),
+        children: [
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: CachedNetworkImage(
+                imageUrl: item.url,
+                cacheKey: item.id,
+                fit: BoxFit.cover,
+                errorWidget: (context, url, error) =>
+                    Center(child: Icon(Icons.image_not_supported)),
+                placeholder: (context, url) => Center(
+                  child: SizedBox(
+                    width: 20,
+                    height: 20,
+                    child: CircularProgressIndicator(),
+                  ),
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 4,vertical: 2),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10),
-                    ),color: Colors.grey
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
                 ),
-                child: Text(
-                  item.name,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: Colors.white),
-                ),
+                color: Colors.grey,
+              ),
+              child: Text(
+                item.name,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(color: Colors.white),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
